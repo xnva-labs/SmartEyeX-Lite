a293d5ef282f9cf9cfbfe4dcf87be86ef91dec2e
xnva-labs <xnvalabs@gmail.com>
2026-06-11 20:51:54 +0700

    Update MainActivity.kt

--- app/src/main/java/com/smarteyex/lite/MainActivity.kt
+++ app/src/main/java/com/smarteyex/lite/MainActivity.kt
@@ -230,7 +230,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             "Santai" -> "Halo $userName! Ada yang bisa gw bantu hari ini? Santai aja ya!"
             else -> "Halo $userName! 🚀 SmartEyeX siap jadi partner hidup lo. Ayo ngobrol, belajar, atau eksplor bareng! Yang kita bicarakan bakal gw ingat semua."
         }
-        addMessage(ChatMessage(welcome, false))
+        addMessage(ChatMessage(content = welcome, is user = false))
         speak(welcome)
     }
 
@@ -242,10 +242,10 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
 
     private fun sendMessage(content: String) {
-        addMessage(ChatMessage(content, true))
+        addMessage(ChatMessage(content = content, is user = true))
         inputMessage.text.clear()
 
-        val loadingMessage = ChatMessage("...", false)
+        val loadingMessage = ChatMessage(content = "...", is user = false)
         chatMessages.add(loadingMessage)
         chatAdapter.notifyItemInserted(chatMessages.size - 1)
 
@@ -293,7 +293,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 runOnUiThread {
                     removeLoadingMessage()
                     val errorMsg = "Maaf $userName, gw error: ${e.message}. Cek API key atau koneksi internet ya!"
-                    addMessage(ChatMessage(errorMsg, false))
+                    addMessage(ChatMessage(content = errorMsg, is user = false))
                     speak(errorMsg)
                 }
             }
@@ -314,7 +314,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
                 runOnUiThread {
                     removeLoadingMessage()
-                    addMessage(ChatMessage(reply, false))
+                    addMessage(ChatMessage(content = reply, is user = false))
                     speak(reply)
                     learnFromConversation(userMessage, reply)
                 }
